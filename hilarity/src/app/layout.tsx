170647512c702cc6815477e0d84ffd9cfd1b11f5
CamdenCall <camden.call3036@stu.mtec.edu>
2025-01-12 23:09:53 -0700

    fonts

--- hilarity/src/app/layout.tsx
+++ hilarity/src/app/layout.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import Head from "next/head";
-
 import Navigation from "@/components/Nav/Nav";
 import "@/styles/global.scss";
+import { Raleway, Montserrat } from 'next/font/google';
+
+
 
 type RootLayoutProps = {
   children: React.ReactNode;
 };
 
+const raleway = Raleway({
+  subsets: ['latin'],
+  weight: 'variable',
+  variable: '--font-raleway',
+});
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: 'variable',
+  variable: '--font-montserrat',
+});
+
 export default function RootLayout({ children }: RootLayoutProps) {
 
   return (
-    <html lang="en">
+    <html lang="en" className={`${raleway.variable} ${montserrat.variable}`}>
       <Head>
         <title>My App</title>
         <meta charSet="utf-8" />
