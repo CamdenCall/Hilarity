814de57034a4e3921d03ccf434d75d3b576b925d
CamdenCall <camden.call3036@stu.mtec.edu>
2025-01-31 21:48:06 -0700

    1/31/25

--- hilarity/src/app/careers/page.tsx
+++ hilarity/src/app/careers/page.tsx
@@ -1,23 +1,27 @@
-import { Metadata } from 'next'
-
-import Header from "@/components/Careers/Header/Header"
-import JobBoard from "@/components/Careers/Jobs/Jobs";
-import Footer from "@/components/Footer/Footer";
+import { Metadata } from 'next';
+import Footer from '@/components/Footer/Footer'
+import JobApplication from '@/components/Careers/Application/Application';
+import { motion } from "motion/react"
 export const metadata: Metadata = {
   title: 'Hilarity Careers',
+  description: 'Hilarity Job Careers',
   icons: {
     icon: "/images/logo.svg",
   }
- }
+}
 export default function Careers() {
-
   return (
-    <>
-      <main>
-        <Header />
-        <JobBoard />
-      </main>
-      <Footer />
-    </>
+   <>
+   <motion.section
+       className="home-header"
+       initial={{ opacity: 0, y: -15 }}
+       whileInView={{ opacity: 1, y: 0 }}
+       transition={ { duration: 0.75 } }
+       viewport={{ once: true }}
+       >
+   <JobApplication />
+   </motion.section>
+    <Footer />
+   </>
   );
 }
