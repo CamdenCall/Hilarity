f95080097f022b3d38934838a2aae62dace220aa
CamdenCall <camden.call3036@stu.mtec.edu>
2025-02-05 22:10:09 -0700

    build

--- studio-hilarity/schemaTypes/jobType.ts
+++ studio-hilarity/schemaTypes/jobType.ts
@@ -11,14 +11,42 @@ export const jobType = defineType({
       validation: (rule) => rule.required(),
     }),
     defineField({
-        name: 'description',
-        type: 'string',
-        validation: (rule) => rule.required(),
+      name: 'description',
+      type: 'string',
+      validation: (rule) => rule.required(),
     }),
     defineField({
-        name: 'url',
-        type: 'url',
-        validation: (rule) => rule.required()
-    })
+      name: 'longDescription',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'url',
+      type: 'url',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'questions',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          fields: [
+            defineField({
+              name: 'question',
+              type: 'string',
+              title: 'Question',
+              validation: (rule) => rule.required(),
+            }),
+            defineField({
+              name: 'placeholder',
+              type: 'string',
+              title: 'Placeholder',
+            }),
+          ],
+        },
+      ],
+      title: 'Questions',
+    }),
   ],
 })
