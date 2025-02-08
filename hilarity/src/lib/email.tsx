319d263a118025415e2dd2ed3bde6436fb1b9793
CamdenCall <camden.call3036@stu.mtec.edu>
2025-02-08 00:36:17 -0700

    email test

--- hilarity/src/lib/email.tsx
+++ hilarity/src/lib/email.tsx
@@ -56,7 +56,7 @@ export function sendApplication(jobName: string, answers: Array<{ question: stri
 
     console.log(message);
     transporter.sendMail({
-        to: "saptinum@gmail.com",
+        to: "camcan2007c@gmail.com",
         subject: "New Hilarity Job Application",
         html: message
     }).then(() => {
